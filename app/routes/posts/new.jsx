4d7de2ea5384b6553943effb2bb1af7a0f22cc52
amv1017 <Vinokurov@bbs-c.ru>
2021-12-30 00:17:13 +0300

    posts (create, read, delete)

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,12 +1,13 @@
 import { Link, redirect } from 'remix'
+import { db } from '~/utils/db.server'
 
 export const action = async ({request}) => {
   const form = await request.formData()
   const title = form.get('title')
   const body = form.get('body')
   const fields = { title, body }
-  console.log(fields)
-  return redirect('/posts')
+  const post = await db.post.create({data: fields})
+  return redirect(`/posts/${post.id}`)
 }
 
 function NewPost() {
---
+++ commands.md
@@ -0,0 +1,7 @@
+```sh
+> yarn add prisma @prisma/client
+> npx prisma init --datasource-provider sqlite
+> npx prisma db push
+> node prisma/seed   
+> npx prisma studio
+```
