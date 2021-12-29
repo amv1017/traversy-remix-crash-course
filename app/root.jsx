4b5505f94d7868107b61e1970ec1fb9041d3c7e1
amv1017 <Vinokurov@bbs-c.ru>
2021-12-29 19:14:55 +0300

    first commit

---
+++ app/root.jsx
@@ -0,0 +1,49 @@
+import { Outlet, LiveReload, Link, Links } from 'remix'
+import globalStylesUrl from '~/styles/global.css'
+
+export const links = () => [{
+  rel: 'stylesheet', href: globalStylesUrl
+}]
+
+export default function App() {
+  return(
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  )
+}
+
+function Document({children, title}) {
+  return(
+    <html lang='en'>
+      <head>
+        <Links />
+        <title>{ title ? title : 'My Remix Blog' }</title>
+      </head>
+      <body>
+        {children}
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
+      </body>
+    </html>
+  )
+}
+
+function Layout({children}) {
+  return(
+    <>
+      <nav className='navbar'>
+        <Link to='/' className='logo'>Remix</Link>
+        <ul className="nav">
+          <li>
+            <Link to='/posts'>Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">
+        {children}
+      </div>
+    </>
+  )
+}
